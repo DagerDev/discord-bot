4834b3a67c077f4e8ecbafd28787c8c82c371b9a
DagerDev <darkerdag@gmail.com>
2026-05-25 18:23:00 +0800

    add play command

---
+++ commands/play.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: "play",
+
+  async execute(message, args) {
+
+    message.channel.send(
+      "Play The CatBurglar here:\nhttps://dagerdev.github.io/game-jamm/"
+    );
+
+  }
+};
